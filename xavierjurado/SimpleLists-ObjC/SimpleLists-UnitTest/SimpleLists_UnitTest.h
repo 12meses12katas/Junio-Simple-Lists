e8917e4ff1b26fa222e904ea0d4edeed4a80087e
Xavier Jurado <xavier.jurado@gmail.com>
2011-06-12 05:57:30 +0800

    Initial project

---
+++ xavierjurado/SimpleLists-ObjC/SimpleLists-UnitTest/SimpleLists_UnitTest.h
@@ -0,0 +1,17 @@
+//
+//  SimpleLists_UnitTest.h
+//  SimpleLists-UnitTest
+//
+//  Created by Xavier Jurado on 11/06/11.
+//  Copyright 2011 Ingens Networks. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface SimpleLists_UnitTest : SenTestCase {
+@private
+    
+}
+
+@end
---
+++ xavierjurado/SimpleLists-ObjC/SimpleLists-UnitTest/SimpleLists_UnitTest.m
@@ -0,0 +1,33 @@
+//
+//  SimpleLists_UnitTest.m
+//  SimpleLists-UnitTest
+//
+//  Created by Xavier Jurado on 11/06/11.
+//  Copyright 2011 Ingens Networks. All rights reserved.
+//
+
+#import "SimpleLists_UnitTest.h"
+
+
+@implementation SimpleLists_UnitTest
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in SimpleLists-UnitTest");
+}
+
+@end
