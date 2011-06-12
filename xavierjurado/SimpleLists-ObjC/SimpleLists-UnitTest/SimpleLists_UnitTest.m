e541387dd30e95d32297c89c7b75592887b462b4
Xavier Jurado <xavier.jurado@gmail.com>
2011-06-13 07:14:58 +0800

    Add tests for Double Linked Lists

--- xavierjurado/SimpleLists-ObjC/SimpleLists-UnitTest/SimpleLists_UnitTest.m
+++ xavierjurado/SimpleLists-ObjC/SimpleLists-UnitTest/SimpleLists_UnitTest.m
@@ -8,53 +8,41 @@
 #import "SimpleLists_UnitTest.h"
 #import "SingleLinkedList.h"
 #import "SingleLinkedNode.h"
+#import "DoubleLinkedList.h"
+#import "DoubleLinkedNode.h"
 
 
 @implementation SimpleLists_UnitTest
 
-- (void)setUp
-{
-    [super setUp];
-    
-    // Set-up code here.
-}
-
-- (void)tearDown
-{
-    // Tear-down code here.
-    
-    [super tearDown];
-}
+/*
+ list = List.new
+ assert_nil(list.find("fred"))
+ list.add("fred")
+ assert_equal("fred", list.find("fred").value())
+ assert_nil(list.find("wilma"))
+ list.add("wilma")
+ assert_equal("fred",  list.find("fred").value())
+ assert_equal("wilma", list.find("wilma").value())
+ assert_equal(["fred", "wilma"], list.values())
+ 
+ list = List.new
+ list.add("fred")
+ list.add("wilma")
+ list.add("betty")
+ list.add("barney")
+ assert_equal(["fred", "wilma", "betty", "barney"], list.values())
+ list.delete(list.find("wilma"))
+ assert_equal(["fred", "betty", "barney"], list.values())
+ list.delete(list.find("barney"))
+ assert_equal(["fred", "betty"], list.values())
+ list.delete(list.find("fred"))
+ assert_equal(["betty"], list.values())
+ list.delete(list.find("betty"))
+ assert_equal([], list.values())
+ */
 
 - (void)testSingleLinkedList
-{
-    /*
-     list = List.new
-     assert_nil(list.find("fred"))
-     list.add("fred")
-     assert_equal("fred", list.find("fred").value())
-     assert_nil(list.find("wilma"))
-     list.add("wilma")
-     assert_equal("fred",  list.find("fred").value())
-     assert_equal("wilma", list.find("wilma").value())
-     assert_equal(["fred", "wilma"], list.values())
-     
-     list = List.new
-     list.add("fred")
-     list.add("wilma")
-     list.add("betty")
-     list.add("barney")
-     assert_equal(["fred", "wilma", "betty", "barney"], list.values())
-     list.delete(list.find("wilma"))
-     assert_equal(["fred", "betty", "barney"], list.values())
-     list.delete(list.find("barney"))
-     assert_equal(["fred", "betty"], list.values())
-     list.delete(list.find("fred"))
-     assert_equal(["betty"], list.values())
-     list.delete(list.find("betty"))
-     assert_equal([], list.values())
-     */
-   
+{   
    SingleLinkedList *list;
    NSArray *array;
    
@@ -90,6 +78,49 @@
    [list remove:[list nodeWithString:@"betty"]];
    array = [NSArray array];
    STAssertEqualObjects(array, [list values], nil);
+   
+   [list release];
+}
+
+- (void)testDoubleLinkedList
+{
+   DoubleLinkedList *list;
+   NSArray *array;
+   
+   list = [[DoubleLinkedList alloc] init];
+   STAssertNil([list nodeWithString:@"fred"], nil);
+   [list add:[DoubleLinkedNode nodeWithString:@"fred"]];
+   STAssertEqualObjects(@"fred", [list nodeWithString:@"fred"].value, nil);
+   STAssertNil([list nodeWithString:@"wilma"], nil);
+   [list add:[DoubleLinkedNode nodeWithString:@"wilma"]];
+   STAssertEqualObjects(@"fred", [list nodeWithString:@"fred"].value, nil);
+   STAssertEqualObjects(@"wilma", [list nodeWithString:@"wilma"].value, nil);
+   array = [NSArray arrayWithObjects:@"fred", @"wilma", nil];
+   STAssertEqualObjects(array, [list values], nil);
+   
+   [list release];
+   
+   list = [[DoubleLinkedList alloc] init];
+   [list add:[DoubleLinkedNode nodeWithString:@"fred"]];
+   [list add:[DoubleLinkedNode nodeWithString:@"wilma"]];
+   [list add:[DoubleLinkedNode nodeWithString:@"betty"]];
+   [list add:[DoubleLinkedNode nodeWithString:@"barney"]];
+   array = [NSArray arrayWithObjects:@"fred", @"wilma", @"betty", @"barney", nil];
+   STAssertEqualObjects(array, [list values], nil);
+   [list remove:[list nodeWithString:@"wilma"]];
+   array = [NSArray arrayWithObjects:@"fred", @"betty", @"barney", nil];
+   STAssertEqualObjects(array, [list values], nil);
+   [list remove:[list nodeWithString:@"barney"]];
+   array = [NSArray arrayWithObjects:@"fred", @"betty", nil];
+   STAssertEqualObjects(array, [list values], nil);
+   [list remove:[list nodeWithString:@"fred"]];
+   array = [NSArray arrayWithObjects:@"betty", nil];
+   STAssertEqualObjects(array, [list values], nil);
+   [list remove:[list nodeWithString:@"betty"]];
+   array = [NSArray array];
+   STAssertEqualObjects(array, [list values], nil);
+   
+   [list release];
 }
 
 @end
