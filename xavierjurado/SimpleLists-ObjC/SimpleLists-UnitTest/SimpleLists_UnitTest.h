5b87bbbcf16fd7e73d553944d66342e30bc92768
Xavier Jurado <xavier.jurado@gmail.com>
2011-06-12 19:21:58 +0800

    First batch of unit tests

--- xavierjurado/SimpleLists-ObjC/SimpleLists-UnitTest/SimpleLists_UnitTest.h
+++ xavierjurado/SimpleLists-ObjC/SimpleLists-UnitTest/SimpleLists_UnitTest.h
@@ -3,7 +3,6 @@
 //  SimpleLists-UnitTest
 //
 //  Created by Xavier Jurado on 11/06/11.
-//  Copyright 2011 Ingens Networks. All rights reserved.
 //
 
 #import <SenTestingKit/SenTestingKit.h>
--- xavierjurado/SimpleLists-ObjC/SimpleLists-UnitTest/SimpleLists_UnitTest.m
+++ xavierjurado/SimpleLists-ObjC/SimpleLists-UnitTest/SimpleLists_UnitTest.m
@@ -3,10 +3,11 @@
 //  SimpleLists-UnitTest
 //
 //  Created by Xavier Jurado on 11/06/11.
-//  Copyright 2011 Ingens Networks. All rights reserved.
 //
 
 #import "SimpleLists_UnitTest.h"
+#import "SingleLinkedList.h"
+#import "SingleLinkedNode.h"
 
 
 @implementation SimpleLists_UnitTest
@@ -25,9 +26,48 @@
     [super tearDown];
 }
 
-- (void)testExample
+- (void)testSingleLinkedList
 {
-    STFail(@"Unit tests are not implemented yet in SimpleLists-UnitTest");
+    /*
+     list = List.new
+     assert_nil(list.find("fred"))
+     list.add("fred")
+     assert_equal("fred", list.find("fred").value())
+     assert_nil(list.find("wilma"))
+     list.add("wilma")
+     assert_equal("fred",  list.find("fred").value())
+     assert_equal("wilma", list.find("wilma").value())
+     assert_equal(["fred", "wilma"], list.values())
+     
+     list = List.new
+     list.add("fred")
+     list.add("wilma")
+     list.add("betty")
+     list.add("barney")
+     assert_equal(["fred", "wilma", "betty", "barney"], list.values())
+     list.delete(list.find("wilma"))
+     assert_equal(["fred", "betty", "barney"], list.values())
+     list.delete(list.find("barney"))
+     assert_equal(["fred", "betty"], list.values())
+     list.delete(list.find("fred"))
+     assert_equal(["betty"], list.values())
+     list.delete(list.find("betty"))
+     assert_equal([], list.values())
+     */
+   
+   SingleLinkedList *list;
+   NSArray *array;
+   
+   list = [[SingleLinkedList alloc] init];
+   STAssertNil([list nodeWithString:@"fred"], nil);
+   [list add:[SingleLinkedNode nodeWithValue:@"fred"]];
+   STAssertEqualObjects(@"fred", [list nodeWithString:@"fred"].value, nil);
+   STAssertNil([list nodeWithString:@"wilma"], nil);
+   [list add:[SingleLinkedNode nodeWithValue:@"wilma"]];
+   STAssertEqualObjects(@"fred", [list nodeWithString:@"fred"].value, nil);
+   STAssertEqualObjects(@"wilma", [list nodeWithString:@"wilma"].value, nil);
+   array = [NSArray arrayWithObjects:@"fred", @"wilma", nil];
+   STAssertEqualObjects(array, [list values], nil);
 }
 
 @end
