af1ed279b38d4de414b2aae7d88402d797559a3b
Silvio René <silvio.rene@gmail.com>
2011-06-13 20:20:52 +0200

    SList

--- remosu/simple_lists.js
+++ remosu/simple_lists.js
@@ -9,11 +9,42 @@ SList.prototype.value = function(){
 
 SList.prototype.find = function(value){
     if(value == this.value() || this.value() == null){
-        return this.value();
+        return this.value() && this;
     } 
     else{
         return this._next.find(value);
     }
 };
+
+SList.prototype.add = function(value){
+    if(this.value() == null){
+        this._value = value;
+        this._next = new SList();
+    }
+    else{
+        this._next.add(value);
+    }
+};
+
+SList.prototype.values = function(){
+    if(this.value() == null){
+        return [];
+    }
+    else{
+        return [this.value()].concat(this._next.values());
+    }
+};
+
+SList.prototype.delete = function(node){
+    if(node.value() == this.value()){
+        this._value = this._next._value; 
+        this._next = this._next._next;
+    }
+    else{
+        this._next.delete(node);
+    }
+}
+
+
 
 exports.SList = SList;
