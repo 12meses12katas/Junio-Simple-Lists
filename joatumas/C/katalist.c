b7177c3a30266250ce56b147dd1397c2927d1e1b
ximotur <ximotur@gmail.com>
2011-06-22 13:26:35 +0200

    added add delete and find_last

--- joatumas/C/katalist.c
+++ joatumas/C/katalist.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "katalist.h"
 
 s_list llista;
@@ -22,6 +24,16 @@ void print_llista(cadena *llista){
 	}
 }
 
+node_t *find_last(){
+	node_t *aux, *ret;
+	aux = ret = llista.first;
+	while(aux != NULL){
+		ret = aux;
+		aux = aux->next;
+	}
+	return(ret);
+}
+
 void print_node(node_t *node){
 	printf("%s\n", node->cad);
 }
@@ -32,11 +44,44 @@ int assert_equal(cadena *llista1, cadena *llista2){
 int assert_nil(node_t *nod){
 }
 
-void add_f(cadena cad){
+void add_f(cadena cad_in){
+	node_t *aux, *aux3;
+	char aux2[MAXCADENA];
+	aux = (node_t *)malloc(sizeof(node_t));
+	strcpy(aux->cad, cad_in);
+	aux->next = NULL;
+	aux->index = max_ind++;
+	//aux->value = value_f;
+	aux3 = find_last();
+	if(aux3 == NULL){
+		//Void list
+		llista.first = aux;
+	}else{
+		aux3->next = aux;
+	}
 }
+
 void delete_f(node_t *node){
+	node_t *act, *ant;
+	act = ant = llista.first;
+	while((act != NULL) && (act->index != node->index)){
+		ant = act;
+		act = act->next;
+	}
+	if(act == ant){
+		//FIRST node
+		llista.first = act.next;
+	}else if(act == NULL){
+		//Node not in list
+	}else{
+		//internode
+		ant->next = act->next;
+	}
+	//Free memory:
+	free(act);
 }
+
 node_t *find_f(cadena cad){
-	return 0;
+	return NULL;
 }
 
