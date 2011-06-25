7f108c14813bc600039c4ecea033a0ac6c1b6845
Alejandro Gómez <alejandroogomez@gmail.com>
2011-06-25 20:51:59 +0200

    tabs to spaces

--- alejandrogomez/simplelists.py
+++ alejandrogomez/simplelists.py
@@ -1,120 +1,120 @@
 class SinglyLinkedList:
-	class Node:
-		def __init__(self, value=None):
-			self.value = value
-			self.next = None	
-		
+    class Node:
+        def __init__(self, value=None):
+            self.value = value
+            self.next = None    
+        
 
-	def __init__(self):
-		self.list = None 
-	
-	def __str__(self):
-		values = self.values()
-		return ", ".join(map(str, values))
-			
-	def add(self, value):
-		"""Adds `value` to the list."""
-		if self.list:
-			node = self.list
-			while node.next:
-				node = node.next
-			node.next = self.Node(value)	
-		else:
-			self.list = self.Node(value)
+    def __init__(self):
+        self.list = None 
+    
+    def __str__(self):
+        values = self.values()
+        return ", ".join(map(str, values))
+            
+    def add(self, value):
+        """Adds `value` to the list."""
+        if self.list:
+            node = self.list
+            while node.next:
+                node = node.next
+            node.next = self.Node(value)    
+        else:
+            self.list = self.Node(value)
 
-	def delete(self, value):
-		"""Deletes all occurences of `value` from the list."""
-		values = self.values()
-		self.list = None
-		for v in filter(lambda v: v != value, values):
-			self.add(v)
+    def delete(self, value):
+        """Deletes all occurences of `value` from the list."""
+        values = self.values()
+        self.list = None
+        for v in filter(lambda v: v != value, values):
+            self.add(v)
 
-	def find(self, value):
-		"""
-		If `value` is in the list, returns the first
-		Node object that has that value.
-		In other case returns None.
-		"""
-		node = self.list
-		while node:
-			if node.value == value:
-				return node
-			node = node.next
-		return node	
-		
-	def length(self):
-		"""Returns the length of the list."""
-		return len(self.values())
-			
-	def values(self):
-		"""Returns a Python list."""
-		list = []
-		node = self.list
-		while node:
-			list.append(node.value)
-			node = node.next
-		return list
+    def find(self, value):
+        """
+        If `value` is in the list, returns the first
+        Node object that has that value.
+        In other case returns None.
+        """
+        node = self.list
+        while node:
+            if node.value == value:
+                return node
+            node = node.next
+        return node 
+        
+    def length(self):
+        """Returns the length of the list."""
+        return len(self.values())
+            
+    def values(self):
+        """Returns a Python list."""
+        list = []
+        node = self.list
+        while node:
+            list.append(node.value)
+            node = node.next
+        return list
 
 class DoublyLinkedList:
-	class Node:
-		def __init__(self, value, prev=None):
-			self.value = value
-			self.prev = prev
-			self.next = None
-	
-	def __init__(self):
-		self.list = None
-	
-	def __str__(self):
-		values = self.values()
-		return ", ".join(map(str, values))
+    class Node:
+        def __init__(self, value, prev=None):
+            self.value = value
+            self.prev = prev
+            self.next = None
+    
+    def __init__(self):
+        self.list = None
+    
+    def __str__(self):
+        values = self.values()
+        return ", ".join(map(str, values))
 
-	def add(self, value):
-		"""Adds `value` to the list."""
-		if self.list:
-			node = self.list
-			while node.next:
-				node = node.next
-			node.next = self.Node(value, node)
-		else:
-			self.list = self.Node(value)
+    def add(self, value):
+        """Adds `value` to the list."""
+        if self.list:
+            node = self.list
+            while node.next:
+                node = node.next
+            node.next = self.Node(value, node)
+        else:
+            self.list = self.Node(value)
 
-	def delete(self, value):
-		"""Deletes all occurrences of `value` from the list."""
-		values = self.values()
-		self.list = None
-		for v in filter(lambda v: v != value, values):
-			self.add(v)
+    def delete(self, value):
+        """Deletes all occurrences of `value` from the list."""
+        values = self.values()
+        self.list = None
+        for v in filter(lambda v: v != value, values):
+            self.add(v)
 
-	def find(self, value):
-		"""
-		If `value` is in the list, returns the first
-		Node object that has that value.
-		In other case returns None.
-		"""
-		node = self.list
-		while node:
-			if node.value == value:
-				return node
-			node = node.next
-		return node
+    def find(self, value):
+        """
+        If `value` is in the list, returns the first
+        Node object that has that value.
+        In other case returns None.
+        """
+        node = self.list
+        while node:
+            if node.value == value:
+                return node
+            node = node.next
+        return node
 
-	def length(self):
-		"""Returns the length of the list."""
-		return len(self.values())	
+    def length(self):
+        """Returns the length of the list."""
+        return len(self.values())   
 
-	def values(self):
-		"""Returns a Python list."""
-		list = []
-		node = self.list
-		while node:
-			list.append(node.value)
-			node = node.next
-		return list
+    def values(self):
+        """Returns a Python list."""
+        list = []
+        node = self.list
+        while node:
+            list.append(node.value)
+            node = node.next
+        return list
 
 
 if __name__ == "__main__":
-	list = SinglyLinkedList()
-	list.add(1)
-	list.add(2)	
-	print list
+    list = SinglyLinkedList()
+    list.add(1)
+    list.add(2) 
+    print list
--- alejandrogomez/testsimplelists.py
+++ alejandrogomez/testsimplelists.py
@@ -3,48 +3,48 @@ import unittest
 from simplelists import SinglyLinkedList, DoublyLinkedList
 
 class SinglyLinkedListTest(unittest.TestCase):
-	def setUp(self): 
-		self.sut = SinglyLinkedList()
+    def setUp(self): 
+        self.sut = SinglyLinkedList()
 
-	def test_singly_linked_list(self):
-		self.assertIsNone(self.sut.find("adam"))
-		self.assertEqual(0, self.sut.length())	
-		self.sut.add("edgar")
-		self.assertEqual(1, self.sut.length())
-		self.sut.add("allen")
-		self.assertEqual(2, self.sut.length())
-		self.assertEqual("edgar", self.sut.find("edgar").value)
-		self.assertEqual(["edgar", "allen"], self.sut.values())	
-		self.sut.delete("edgar")
-		self.assertIsNone(self.sut.find("edgar"))
-		self.assertEqual(1, self.sut.length())
-		self.sut.delete("adam")
-		self.assertEqual(1, self.sut.length())
-		self.sut.delete("allen")
-		self.assertEqual(0, self.sut.length())
+    def test_singly_linked_list(self):
+        self.assertIsNone(self.sut.find("adam"))
+        self.assertEqual(0, self.sut.length())  
+        self.sut.add("edgar")
+        self.assertEqual(1, self.sut.length())
+        self.sut.add("allen")
+        self.assertEqual(2, self.sut.length())
+        self.assertEqual("edgar", self.sut.find("edgar").value)
+        self.assertEqual(["edgar", "allen"], self.sut.values()) 
+        self.sut.delete("edgar")
+        self.assertIsNone(self.sut.find("edgar"))
+        self.assertEqual(1, self.sut.length())
+        self.sut.delete("adam")
+        self.assertEqual(1, self.sut.length())
+        self.sut.delete("allen")
+        self.assertEqual(0, self.sut.length())
 
 
 class DoublyLinkedListTest(unittest.TestCase):
-	def setUp(self): 
-		self.sut = DoublyLinkedList()
+    def setUp(self): 
+        self.sut = DoublyLinkedList()
 
-	def test_doubly_linked_list(self):
-		self.assertIsNone(self.sut.find("adam"))
-		self.assertEqual(0, self.sut.length())	
-		self.sut.add("edgar")
-		self.assertEqual(1, self.sut.length())
-		self.sut.add("allen")
-		self.assertEqual(2, self.sut.length())
-		self.assertEqual("edgar", self.sut.find("edgar").value)
-		self.assertEqual(["edgar", "allen"], self.sut.values())	
-		self.sut.delete("edgar")
-		self.assertIsNone(self.sut.find("edgar"))
-		self.assertEqual(1, self.sut.length())
-		self.sut.delete("adam")
-		self.assertEqual(1, self.sut.length())
-		self.sut.delete("allen")
-		self.assertEqual(0, self.sut.length())
+    def test_doubly_linked_list(self):
+        self.assertIsNone(self.sut.find("adam"))
+        self.assertEqual(0, self.sut.length())  
+        self.sut.add("edgar")
+        self.assertEqual(1, self.sut.length())
+        self.sut.add("allen")
+        self.assertEqual(2, self.sut.length())
+        self.assertEqual("edgar", self.sut.find("edgar").value)
+        self.assertEqual(["edgar", "allen"], self.sut.values()) 
+        self.sut.delete("edgar")
+        self.assertIsNone(self.sut.find("edgar"))
+        self.assertEqual(1, self.sut.length())
+        self.sut.delete("adam")
+        self.assertEqual(1, self.sut.length())
+        self.sut.delete("allen")
+        self.assertEqual(0, self.sut.length())
 
 
 if __name__ == "__main__":
-	unittest.main()	 
+    unittest.main()  
