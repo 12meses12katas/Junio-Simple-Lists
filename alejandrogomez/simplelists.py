412a3d077f7fc694d8772928581bcb0814a26e92
Alejandro Gómez <alejandroogomez@gmail.com>
2011-06-27 15:30:42 +0200

    MyList implementation added (with test).

--- alejandrogomez/simplelists.py
+++ alejandrogomez/simplelists.py
@@ -127,3 +127,72 @@ class DoublyLinkedList:
     def values(self):
         """Returns a Python list."""
         return list(self)
+
+class MyList:
+    def __init__(self, value=None):
+        self._value = value
+        self._next = None
+        
+    def __str__(self):
+        if self._next:
+            return str(self._value) + ", " + str(self._next)
+        elif self._value:
+            return str(self._value)
+        else:
+            return "" 
+        
+    def elem(self, index):
+        """Returns the value at `index` if exists. If not returns None."""
+        if index == 1:
+            return self._value
+        elif self._next and index > 0:
+            return self._next.elem(index - 1)
+        else:
+            return None
+    
+    def add(self, value):
+        """Add `value` to the bottom of the list."""
+        if self._next:
+            self._next.add(value)
+        elif self._value:
+            self._next = MyList(value)
+        else:
+            self._value = value
+            
+    def delete(self, value):
+        """Delete all occurrences of `value` from the list."""
+        node = self.find(value)
+        while node:
+            if node._next:
+                node._value = node._next._value
+                node._next = node._next._next
+            else:
+                node._value = None
+            node = self.find(value)
+        
+    def find(self, value):
+        """Returns the first node with `value`, if any. If not returns None."""
+        if self._value == value:
+            return self
+        elif self._next:
+            return self._next.find(value)
+        else:
+            return None
+                 
+    def length(self):
+        """Length of the list."""
+        if self._next:
+            return 1 + self._next.length()
+        elif self._value:
+            return 1
+        else:
+            return 0 
+
+    def values(self):
+        """Returns a Python list."""
+        if self._next:
+            return [self._value] + self._next.values()
+        elif self._value:
+            return [self._value]
+        else:
+            return []
--- alejandrogomez/testsimplelists.py
+++ alejandrogomez/testsimplelists.py
@@ -1,6 +1,6 @@
 import unittest
 
-from simplelists import SinglyLinkedList, DoublyLinkedList
+from simplelists import SinglyLinkedList, DoublyLinkedList, MyList
 
 class SinglyLinkedListTest(unittest.TestCase):
     def setUp(self): 
@@ -13,14 +13,14 @@ class SinglyLinkedListTest(unittest.TestCase):
         self.assertEqual(1, self.sut.length())
         self.sut.add("allen")
         self.assertEqual(2, self.sut.length())
-        self.assertEqual("edgar", self.sut.find("edgar").value)
+        self.assertEqual("edgar", self.sut.find("edgar")._value)
         self.assertEqual(["edgar", "allen"], self.sut.values()) 
-        self.sut.delete("edgar")
-        self.assertIsNone(self.sut.find("edgar"))
+        self.sut.delete("allen")
+        self.assertIsNone(self.sut.find("allen"))
         self.assertEqual(1, self.sut.length())
         self.sut.delete("adam")
         self.assertEqual(1, self.sut.length())
-        self.sut.delete("allen")
+        self.sut.delete("edgar")
         self.assertEqual(0, self.sut.length())
 
 
@@ -28,23 +28,42 @@ class DoublyLinkedListTest(unittest.TestCase):
     def setUp(self): 
         self.sut = DoublyLinkedList()
 
-    def test_doubly_linked_list(self):
+    def test_singly_linked_list(self):
         self.assertIsNone(self.sut.find("adam"))
         self.assertEqual(0, self.sut.length())  
         self.sut.add("edgar")
         self.assertEqual(1, self.sut.length())
         self.sut.add("allen")
         self.assertEqual(2, self.sut.length())
-        self.assertEqual("edgar", self.sut.find("edgar").value)
+        self.assertEqual("edgar", self.sut.find("edgar")._value)
         self.assertEqual(["edgar", "allen"], self.sut.values()) 
-        self.sut.delete("edgar")
-        self.assertIsNone(self.sut.find("edgar"))
+        self.sut.delete("allen")
+        self.assertIsNone(self.sut.find("allen"))
         self.assertEqual(1, self.sut.length())
         self.sut.delete("adam")
         self.assertEqual(1, self.sut.length())
-        self.sut.delete("allen")
+        self.sut.delete("edgar")
         self.assertEqual(0, self.sut.length())
 
+class MyListTest(unittest.TestCase):
+    def setUp(self): 
+        self.sut = MyList()
 
+    def test_singly_linked_list(self):
+        self.assertIsNone(self.sut.find("adam"))
+        self.assertEqual(0, self.sut.length())  
+        self.sut.add("edgar")
+        self.assertEqual(1, self.sut.length())
+        self.sut.add("allen")
+        self.assertEqual(2, self.sut.length())
+        self.assertEqual("edgar", self.sut.find("edgar")._value)
+        self.assertEqual(["edgar", "allen"], self.sut.values()) 
+        self.sut.delete("allen")
+        self.assertIsNone(self.sut.find("allen"))
+        self.assertEqual(1, self.sut.length())
+        self.sut.delete("adam")
+        self.assertEqual(1, self.sut.length())
+        self.sut.delete("edgar")
+        self.assertEqual(0, self.sut.length())
 if __name__ == "__main__":
     unittest.main()  
